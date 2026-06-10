cd465b16fc241b4b526874b9c69844628dd90d27
bladedaniels <bladedaniels@hotmail.com>
2026-06-09 20:25:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib import colors
 import io
